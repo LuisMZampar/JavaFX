f728ceb4acaa60cc9d8ac06ff07b46328ccd25b9
LuisMZampar <rm550531@fiap.com.br>
2023-10-03 20:45:35 -0300

    Adicionando a Api

---
+++ javafx-demo-main/src/main/java/com/example/Personagem.java
@@ -0,0 +1,97 @@
+package com.example;
+
+public class Personagem {
+
+    private int id;
+    private String url;
+    private String name;
+    private String sourceUrl;
+    private String films;
+    private String shortFilms;
+    private String tvShows;
+    private String videoGames;
+    private String alignment;
+    private String parkAttractions;
+    private String allies;
+    private String enemies;
+
+    public Personagem(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+    public String getFilms() {
+        return films;
+    }
+    public void setFilms(String films) {
+        this.films = films;
+    }
+    public String getShortFilms() {
+        return shortFilms;
+    }
+    public void setShortFilms(String shortFilms) {
+        this.shortFilms = shortFilms;
+    }
+    public String getTvShows() {
+        return tvShows;
+    }
+    public void setTvShows(String tvShows) {
+        this.tvShows = tvShows;
+    }
+    public String getVideoGames() {
+        return videoGames;
+    }
+    public void setVideoGames(String videoGames) {
+        this.videoGames = videoGames;
+    }
+    public String getAlignment() {
+        return alignment;
+    }
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+    public String getParkAttractions() {
+        return parkAttractions;
+    }
+    public void setParkAttractions(String parkAttractions) {
+        this.parkAttractions = parkAttractions;
+    }
+    public String getAllies() {
+        return allies;
+    }
+    public void setAllies(String allies) {
+        this.allies = allies;
+    }
+    public String getEnemies() {
+        return enemies;
+    }
+    public void setEnemies(String enemies) {
+        this.enemies = enemies;
+    }
+
+
+}
+
